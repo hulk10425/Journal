8ab178feebab117d9eaba4aed1e819f0888703d0
hulk10425 <hulk104255@gmail.com>
2017-08-04 10:32:53 +0800

    finish the choose photo func

--- Journal/Journal/JournalViewController.swift
+++ Journal/Journal/JournalViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class JournalViewController: UIViewController {
+class JournalViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let imagePicker = UIImagePickerController()
 
     @IBAction func chosePhoto(_ sender: UIButton) {
-        
-        
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
     }
+    
+    @IBOutlet weak var journalContent: UITextView!
+    @IBOutlet weak var journalTitle: UILabel!
+    @IBOutlet weak var journalPhotoView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        //picker.dismiss(animated: true, completion: nil)
+        
+    
+    if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+        journalPhotoView.image = image
+    } else if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        journalPhotoView.image = image
+    }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 
 
